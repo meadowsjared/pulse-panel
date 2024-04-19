157dfbf084ba7553258953c8c030c0961b454df7
Jared Meadows <meadowsjared@gmail.com>
2024-04-19 10:14:17 -0700

    update: vite.config.ts for new vite-plugin-rewrite-all plugin
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import components from 'unplugin-vue-components/vite';
-import pluginRewriteAll from 'vite-plugin-rewrite-all';
-import autoImport from 'unplugin-auto-import/vite';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import components from 'unplugin-vue-components/vite'
+import pluginRewriteAll from '@evg3/vite-plugin-rewrite-all'
+import autoImport from 'unplugin-auto-import/vite'
 
-const viteEnv = {};
+const viteEnv = {}
 Object.keys(process.env).forEach(key => {
   if (key.startsWith('VITE_')) {
-    viteEnv[`import.meta.env.${key}`] = process.env[key];
+    viteEnv[`import.meta.env.${key}`] = process.env[key]
   }
-});
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -27,4 +27,4 @@ export default defineConfig({
   ],
   base: './',
   server: { port: 3000 },
-});
+})
