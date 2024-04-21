017edeb53bdac03d72ffaf6b31f59139cf6bef21
Jared Meadows <meadowsjared@gmail.com>
2024-04-21 04:50:08 -0700

    chore: added menu to main.js to allow for customizing the menu
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/electron/main/main.js
+++ src/electron/main/main.js
@@ -1,8 +1,58 @@
-const { join } = require('path');
-const { app, BrowserWindow } = require('electron');
+const { join } = require('path')
+const { app, BrowserWindow, Menu } = require('electron')
 
-const isDev = process.env.npm_lifecycle_event === 'app:dev' ? true : false;
-process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true';
+const isDev = process.env.npm_lifecycle_event === 'app:dev'
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true'
+
+app.whenReady().then(() => {
+  const template = [
+    {
+      label: 'File',
+      submenu: [{ role: 'quit' }],
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { type: 'separator' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Settings',
+          click: () => {
+            console.log('Opening settings...')
+            // Here you can add code to open your settings window or panel
+            openSettingsWindow()
+          },
+        },
+      ],
+    },
+    {
+      label: 'View',
+      submenu: [{ role: 'reload' }, { role: 'toggledevtools' }],
+    },
+    {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click: async () => {
+            const { shell } = require('electron')
+            await shell.openExternal('https://electronjs.org')
+          },
+        },
+      ],
+    },
+  ]
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+})
 
 function createWindow() {
   // Create the browser window.
@@ -13,14 +63,14 @@ function createWindow() {
       preload: join(__dirname, '../preload/preload.js'),
       nodeIntegration: true,
     },
-  });
+  })
 
   // and load the index.html of the app.
   if (isDev) {
-    mainWindow.loadURL('http://localhost:3000'); // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    mainWindow.loadURL('http://localhost:3000') // Open the DevTools.
+    mainWindow.webContents.openDevTools()
   } else {
-    mainWindow.loadFile(join(__dirname, '../../index.html'));
+    mainWindow.loadFile(join(__dirname, '../../index.html'))
   }
   // mainWindow.loadURL( //this doesn't work on macOS in build and preview mode
   //     isDev ?
@@ -33,19 +83,20 @@ function createWindow() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow();
+  createWindow()
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
+
