8c055203b68cbbf96c7119951c8801f01b6f797c
Jared Meadows <meadowsjared@gmail.com>
2024-04-21 04:40:42 -0700

    chore: move main content from Index.vue to Main.vue that way you can add other pages that look different
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/Index.vue
+++ src/Index.vue
@@ -1,21 +1,5 @@
 <template>
-  <div class="e-nuxt-container">
-    <h1 class="title text-3xl font-semibold text-arubaGreen pt-5">Electron Test App</h1>
-    <div class="e-nuxt-content">
-      <div class="e-nuxt-logo">
-        <img style="max-width: 100%" src="./assets/electron.png" alt="electron icon" />
-      </div>
-      <div class="e-nuxt-system-info bg-gray-100">
-        <Header msg="test" />
-        <router-view />
-      </div>
-    </div>
-    <div class="e-nuxt-links">
-      <div class="e-nuxt-button" @click="openURL('https://github.com/michalzaq12/electron-nuxt')">Github</div>
-      <div class="e-nuxt-button" @click="openURL('https://nuxtjs.org/guide')">Nuxt.js</div>
-      <div class="e-nuxt-button" @click="openURL('https://electronjs.org/docs')">Electron.js</div>
-    </div>
-  </div>
+  <router-view />
 </template>
 
 <script setup lang="ts">
@@ -42,50 +26,3 @@ function openURL(url: string) {
 //   },
 // };
 </script>
-
-<style>
-.e-nuxt-container {
-  min-height: calc(100vh - 50px);
-  background: linear-gradient(to right, #ece9e6, #ffffff);
-  font-family: Helvetica, sans-serif;
-}
-
-.e-nuxt-content {
-  display: flex;
-  justify-content: space-around;
-  padding-top: 50px;
-  align-items: flex-start;
-  flex-wrap: wrap;
-}
-
-.e-nuxt-logo {
-  width: 400px;
-}
-
-.e-nuxt-system-info {
-  padding: 20px;
-  border-top: 1px solid #397c6d;
-  border-bottom: 1px solid #397c6d;
-}
-
-.e-nuxt-links {
-  padding: 25px 0;
-  display: flex;
-  justify-content: center;
-}
-
-.e-nuxt-button {
-  color: #364758;
-  padding: 5px 20px;
-  border: 1px solid #397c6d;
-  margin: 0 20px;
-  border-radius: 15px;
-  font-size: 1rem;
-}
-
-.e-nuxt-button:hover {
-  cursor: pointer;
-  color: white;
-  background-color: #397c6d;
-}
-</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,22 +1,73 @@
 <script setup lang="ts">
-import InlineSvg from "vue-inline-svg";
-import addIcon from '../assets/images/streamlinehq-add-thick-interface-essential-50.svg';
-import subtractIcon from '../assets/images/streamlinehq-subtract-thick-interface-essential-50.svg';
-import { useCounterStore } from '../store/counter';
-
-const counterStore = useCounterStore();
+function openURL(url: string) {
+  window.open(url);
+}
 </script>
 
 <template>
-  <div class="flex items-center justify-center gap-2">
-    count is: {{ counterStore.count }}
-    <button type="button" @click="counterStore.add()" class="bg-arubaGreen rounded px-3 py-1 my-4 text-white active:bg-greenSeduction active:text-neutralLight-400">
-      <inline-svg :src="addIcon" class="w-6 h-6" />
-    </button>
-    <button type="button" @click="counterStore.subtract()" class="bg-arubaGreen rounded px-3 py-1 my-4 text-white active:bg-greenSeduction active:text-neutralLight-400">
-      <inline-svg :src="subtractIcon" class="w-6 h-6" />
-    </button>
+  <div class="e-nuxt-container">
+    <h1 class="title text-3xl font-semibold text-arubaGreen pt-5">Electron Test App</h1>
+    <div class="e-nuxt-content">
+      <div class="e-nuxt-logo">
+        <img style="max-width: 100%" src="../assets/electron.png" alt="electron icon" />
+      </div>
+      <div class="e-nuxt-system-info bg-gray-100">
+        <Header msg="test" />
+        <Counter />
+        <router-view />
+      </div>
+    </div>
+    <div class="e-nuxt-links">
+      <div class="e-nuxt-button" @click="openURL('https://github.com/michalzaq12/electron-nuxt')">Github</div>
+      <div class="e-nuxt-button" @click="openURL('https://nuxtjs.org/guide')">Nuxt.js</div>
+      <div class="e-nuxt-button" @click="openURL('https://electronjs.org/docs')">Electron.js</div>
+    </div>
   </div>
-
-  <router-view />
 </template>
+
+<style>
+.e-nuxt-container {
+  min-height: calc(100vh - 50px);
+  background: linear-gradient(to right, #ece9e6, #ffffff);
+  font-family: Helvetica, sans-serif;
+}
+
+.e-nuxt-content {
+  display: flex;
+  justify-content: space-around;
+  padding-top: 50px;
+  align-items: flex-start;
+  flex-wrap: wrap;
+}
+
+.e-nuxt-logo {
+  width: 400px;
+}
+
+.e-nuxt-system-info {
+  padding: 20px;
+  border-top: 1px solid #397c6d;
+  border-bottom: 1px solid #397c6d;
+}
+
+.e-nuxt-links {
+  padding: 25px 0;
+  display: flex;
+  justify-content: center;
+}
+
+.e-nuxt-button {
+  color: #364758;
+  padding: 5px 20px;
+  border: 1px solid #397c6d;
+  margin: 0 20px;
+  border-radius: 15px;
+  font-size: 1rem;
+}
+
+.e-nuxt-button:hover {
+  cursor: pointer;
+  color: white;
+  background-color: #397c6d;
+}
+</style>
