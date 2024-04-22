16ee5f91b3c49213b55ec1b56c4cac066df29eaf
Jared Meadows <meadowsjared@gmail.com>
2024-04-22 12:13:47 -0700

    style: update format on preload.js
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/electron/preload/preload.js
+++ src/electron/preload/preload.js
@@ -2,16 +2,16 @@
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
 
   for (const dependency of ['chrome', 'node', 'electron']) {
-    replaceText(`${dependency}-version`, process.versions[dependency]);
+    replaceText(`${dependency}-version`, process.versions[dependency])
   }
-});
+})
 
-const { contextBridge } = require('electron');
+const { contextBridge } = require('electron')
 
 contextBridge.exposeInMainWorld('electron', {
   versions: {
@@ -22,4 +22,4 @@ contextBridge.exposeInMainWorld('electron', {
     vue: require('vue/package.json').version,
     pinia: require('pinia/package.json').version,
   },
-});
+})
