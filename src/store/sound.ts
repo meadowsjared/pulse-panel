32cb0b482155b6d41519416ff44949ad2e5eca53
Jared Meadows <meadowsjared@gmail.com>
2024-04-28 16:30:50 -0700

    feat: playSound now doesn't resolve until sound ends also made playSound easier to use, by making everything optional
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/store/sound.ts
+++ src/store/sound.ts
@@ -7,6 +7,7 @@ interface State {
   currentAudio: HTMLAudioElement | null
   playingAudio: boolean
   numSoundsPlaying: number
+  currentSoundResolve: (() => void) | null
 }
 
 export const useSoundStore = defineStore('sound', {
@@ -15,6 +16,7 @@ export const useSoundStore = defineStore('sound', {
     currentAudio: null,
     playingAudio: false,
     numSoundsPlaying: 0,
+    currentSoundResolve: null,
   }),
   actions: {
     /**
@@ -31,41 +33,56 @@ export const useSoundStore = defineStore('sound', {
      * @param audioFile the file to play, defaults to `chordAlert`
      */
     async playSound(
-      outputDeviceId: string,
-      volume: number | null = null,
-      audioFile: string = chordAlert
+      audioFile: string | null = chordAlert,
+      outputDeviceId: string | null = null,
+      volume: number | null = null
     ): Promise<void> {
-      const settingsStore = useSettingsStore()
-      if (settingsStore.allowOverlappingSound === false && this.currentAudio && this.playingAudio) {
-        this.currentAudio.pause()
-        this.currentAudio.currentTime = 0
-        this.numSoundsPlaying--
-      }
-
-      this.currentAudio = new Audio(audioFile)
-      this.currentAudio.src = chordAlert
-
-      await this.currentAudio.setSinkId(outputDeviceId).catch((error: any) => {
-        let errorMessage = error
-        if (error.name === 'SecurityError') {
-          errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`
+      // return a promise
+      return new Promise(async resolve => {
+        const settingsStore = useSettingsStore()
+        if (settingsStore.allowOverlappingSound === false && this.currentAudio && this.playingAudio) {
+          this.currentAudio.pause()
+          this.currentAudio.currentTime = 0
+          this.numSoundsPlaying--
+          if (this.currentSoundResolve) {
+            this.currentSoundResolve()
+            this.currentSoundResolve = null
+          }
         }
-        console.error(errorMessage)
+
+        this.currentAudio = new Audio(audioFile ?? chordAlert)
+
+        if (!outputDeviceId) {
+          outputDeviceId = settingsStore.outputDeviceId
+          if (!outputDeviceId) {
+            return
+          }
+        }
+
+        await this.currentAudio.setSinkId(outputDeviceId).catch((error: any) => {
+          let errorMessage = error
+          if (error.name === 'SecurityError') {
+            errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`
+          }
+          console.error(errorMessage)
+        })
+        this.currentAudio.volume = volume ?? this.volume // set the volume to max
+        this.currentAudio.onplaying = () => {
+          this.playingAudio = true
+        }
+        this.numSoundsPlaying++
+        this.currentAudio.play()
+        this.currentSoundResolve = resolve // store this so we can end it early if we want to
+        this.currentAudio.onended = () => {
+          this.currentAudio?.remove()
+          this.currentAudio = null
+          this.numSoundsPlaying--
+          if (this.numSoundsPlaying < 1) {
+            this.playingAudio = false
+          }
+          resolve()
+        }
       })
-      this.currentAudio.volume = volume ?? this.volume // set the volume to max
-      this.currentAudio.onplaying = () => {
-        this.playingAudio = true
-      }
-      this.numSoundsPlaying++
-      this.currentAudio.play()
-      this.currentAudio.onended = () => {
-        this.currentAudio?.remove()
-        this.currentAudio = null
-        this.numSoundsPlaying--
-        if (this.numSoundsPlaying < 1) {
-          this.playingAudio = false
-        }
-      }
     },
   },
 })
