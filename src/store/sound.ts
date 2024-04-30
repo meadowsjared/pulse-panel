16bc5f0308fcb47afa378be53cc63f7b7701d2b2
Jared Meadows <meadowsjared@gmail.com>
2024-04-30 05:47:53 -0700

    refactor: soundStore.playSound function to support Sound objects
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/store/sound.ts
+++ src/store/sound.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
 import chordAlert from '../assets/wav/457518__graham_makes__chord-alert-notification.wav'
 import { useSettingsStore } from './settings'
+import { Sound } from '@/@types/sound'
 
 interface State {
   volume: number
@@ -33,55 +34,63 @@ export const useSoundStore = defineStore('sound', {
      * @param audioFile the file to play, defaults to `chordAlert`
      */
     async playSound(
-      audioFile: string | null = chordAlert,
+      audioFile: Sound | null = null,
       outputDeviceId: string | null = null,
       volume: number | null = null
     ): Promise<void> {
       // return a promise
-      return new Promise(async resolve => {
-        const settingsStore = useSettingsStore()
-        if (settingsStore.allowOverlappingSound === false && this.currentAudio && this.playingAudio) {
-          this.currentAudio.pause()
-          this.currentAudio.currentTime = 0
-          this.numSoundsPlaying--
-          if (this.currentSoundResolve) {
-            this.currentSoundResolve()
-            this.currentSoundResolve = null
+      return new Promise(resolve => {
+        ;(async () => {
+          const settingsStore = useSettingsStore()
+          if (settingsStore.allowOverlappingSound === false && this.currentAudio && this.playingAudio) {
+            this.currentAudio.pause()
+            this.currentAudio.currentTime = 0
+            this.numSoundsPlaying--
+            if (this.currentSoundResolve) {
+              this.currentSoundResolve()
+              this.currentSoundResolve = null
+            }
           }
-        }
 
-        this.currentAudio = new Audio(audioFile ?? chordAlert)
+          if (audioFile?.audioUrl === undefined && audioFile?.path !== undefined) {
+            const audioUrl = await settingsStore.getFile(audioFile.path)
+            if (audioUrl) {
+              audioFile.audioUrl = audioUrl
+            }
+          }
+          this.currentAudio = new Audio(audioFile?.audioUrl ?? chordAlert)
 
-        if (!outputDeviceId) {
-          outputDeviceId = settingsStore.outputDeviceId
           if (!outputDeviceId) {
-            return
+            outputDeviceId = settingsStore.outputDeviceId
+            if (!outputDeviceId) {
+              return
+            }
           }
-        }
 
-        await this.currentAudio.setSinkId(outputDeviceId).catch((error: any) => {
-          let errorMessage = error
-          if (error.name === 'SecurityError') {
-            errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`
+          await this.currentAudio.setSinkId(outputDeviceId).catch((error: any) => {
+            let errorMessage = error
+            if (error.name === 'SecurityError') {
+              errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`
+            }
+            console.error(errorMessage)
+          })
+          this.currentAudio.volume = volume ?? this.volume // set the volume to max
+          this.currentAudio.onplaying = () => {
+            this.playingAudio = true
           }
-          console.error(errorMessage)
-        })
-        this.currentAudio.volume = volume ?? this.volume // set the volume to max
-        this.currentAudio.onplaying = () => {
-          this.playingAudio = true
-        }
-        this.numSoundsPlaying++
-        this.currentAudio.play()
-        this.currentSoundResolve = resolve // store this so we can end it early if we want to
-        this.currentAudio.onended = () => {
-          this.currentAudio?.remove()
-          this.currentAudio = null
-          this.numSoundsPlaying--
-          if (this.numSoundsPlaying < 1) {
-            this.playingAudio = false
+          this.numSoundsPlaying++
+          this.currentAudio.play()
+          this.currentSoundResolve = resolve // store this so we can end it early if we want to
+          this.currentAudio.onended = () => {
+            this.currentAudio?.remove()
+            this.currentAudio = null
+            this.numSoundsPlaying--
+            if (this.numSoundsPlaying < 1) {
+              this.playingAudio = false
+            }
+            resolve()
           }
-          resolve()
-        }
+        })()
       })
     },
   },
