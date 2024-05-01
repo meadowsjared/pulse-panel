0f45d9fdde26ed36b8f5136fb7dc996871aa4d0c
Jared Meadows <meadowsjared@gmail.com>
2024-05-01 15:56:20 -0700

    refactor: settings to save/fetch output devices as an array 🤘🎉 now it supports multiple outputs
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/store/settings.ts
+++ src/store/settings.ts
@@ -4,9 +4,10 @@ import { Sound } from '@/@types/sound'
 import { openDB } from 'idb'
 import { File } from '@/@types/file'
 import { v4 } from 'uuid'
+import { useSoundStore } from './sound'
 
 interface State {
-  outputDeviceId: string | null
+  outputDevices: string[]
   darkMode: boolean
   allowOverlappingSound: boolean
   sounds: Sound[]
@@ -15,13 +16,15 @@ interface State {
 }
 
 type BooleanSettings = 'darkMode' | 'allowOverlappingSound'
-type StringSettings = 'outputDeviceId'
-type ArraySettings = 'sounds'
+type ArraySoundSettings = 'sounds'
+type ArraySettings = 'outputDevices'
 export type DisplayMode = 'edit' | 'play'
+
+export interface SettingsStore extends ReturnType<typeof useSettingsStore> {}
 
 export const useSettingsStore = defineStore('settings', {
   state: (): State => ({
-    outputDeviceId: null,
+    outputDevices: [],
     darkMode: true,
     allowOverlappingSound: false,
     sounds: [],
@@ -37,61 +40,48 @@ export const useSettingsStore = defineStore('settings', {
       this.displayMode = this.displayMode === 'play' ? 'edit' : 'play'
     },
     /**
-     * Fetch a string setting from the store
-     * @param key the key it's saved under
-     * @param defaultValue the default value if it's not set, default to ''
-     * @returns the value of the setting
-     */
-    async fetchStringSetting(key: StringSettings, defaultValue: string = ''): Promise<string> {
-      const electron: Settings | undefined = window.electron
-      const returnedString = await electron?.readSetting?.(key)
-      if (returnedString === undefined || typeof returnedString !== 'string') {
-        // automagically set the defaultValue if key is 'outputDeviceId'
-        if (key === 'outputDeviceId') {
-          const devices = await navigator.mediaDevices.enumerateDevices()
-          const audioOutputDevices = devices.filter(device => device.kind === 'audiooutput')
-          defaultValue = audioOutputDevices.length > 0 ? audioOutputDevices[0].deviceId : 'default'
-        }
-        await electron?.saveSetting?.(key, defaultValue)
-        this[key] = defaultValue
-      } else {
-        this[key] = returnedString
-      }
-
-      return this[key] || ''
-    },
-    /**
-     * Save a string setting to the store
-     * and validate the value if key is outputDeviceId
-     * @param key the key it's saved under
-     * @param value the value to save
-     * @returns true if saved successfully
-     */
-    async saveString(key: StringSettings, value: string): Promise<boolean> {
-      const electron: Settings | undefined = window.electron
-      if (key === 'outputDeviceId') {
-        // Check if the device exists
-        const devices = await navigator.mediaDevices.enumerateDevices()
-        const device = devices.find(device => device.deviceId === value)
-        if (!device) {
-          console.error('Device not found')
-          return false // failed to save
-        }
-      }
-      await electron?.saveSetting?.(key, value)
-      this[key] = value
-      return true // saved successfully
-    },
-    /**
      * Save an array setting to the store
      * @param key the key it's saved under
      * @param value the value to save
      * @returns true if saved successfully
      */
-    async saveArray(key: ArraySettings, value: (Object | null)[]): Promise<boolean> {
+    async saveStringArray(key: ArraySettings, value: string[]): Promise<boolean> {
       const electron: Settings | undefined = window.electron
       await electron?.saveSetting?.(key, JSON.stringify(value))
-      this[key] = value as Sound[]
+      this[key] = value
+      return true
+    },
+    /**
+     * Fetch an array setting from the store
+     * @param key the key it's saved under
+     * @param defaultValue the default value if it's not set, default to []
+     * @returns the value of the setting
+     */
+    async fetchStringArray(key: ArraySettings, defaultValue?: string[]): Promise<string[]> {
+      const soundStore = useSoundStore()
+      const electron: Settings | undefined = window.electron
+      const returnedArray = await electron?.readSetting?.(key)
+      if (returnedArray === undefined) {
+        if (key === 'outputDevices') {
+          const devices = await navigator.mediaDevices.enumerateDevices()
+          const audioOutputDevices = devices.filter(device => device.kind === 'audiooutput')
+          defaultValue = [audioOutputDevices.length > 0 ? audioOutputDevices[0].deviceId : 'default']
+        }
+        await electron?.saveSetting?.(key, JSON.stringify(defaultValue))
+        this[key] = defaultValue ?? []
+      } else if (typeof returnedArray === 'string') {
+        this[key] = JSON.parse(returnedArray)
+      }
+      if (key === 'outputDevices') {
+        soundStore.populatePlayingAudio(this[key].length)
+      }
+
+      return this[key]
+    },
+    async saveSoundArray(key: ArraySoundSettings, value: Sound[]): Promise<boolean> {
+      const electron: Settings | undefined = window.electron
+      await electron?.saveSetting?.(key, JSON.stringify(value))
+      this[key] = value
       return true
     },
     /**
@@ -100,7 +90,7 @@ export const useSettingsStore = defineStore('settings', {
      * @param defaultValue the default value if it's not set, default to [{ id: v4() }]
      * @returns the value of the setting
      */
-    async fetchArraySetting(key: ArraySettings, defaultValue: Sound[] = [{ id: v4() }]): Promise<Sound[]> {
+    async fetchSoundSetting(key: ArraySoundSettings, defaultValue: Sound[] = [{ id: v4() }]): Promise<Sound[]> {
       const electron: Settings | undefined = window.electron
       const returnedArray = await electron?.readSetting?.(key)
       if (returnedArray === undefined || typeof returnedArray !== 'string') {
@@ -117,16 +107,17 @@ export const useSettingsStore = defineStore('settings', {
      * @param sounds the array of sounds
      * @returns the array of sounds with image URLs
      */
-    async _getImageUrls(key: ArraySettings, sounds: Sound[]): Promise<Sound[]> {
-      for (const sound of sounds) {
-        if (sound.imageUrl === undefined && sound.imagePath !== undefined) {
-          const imageUrl = await this.getFile(sound.imagePath)
-          if (imageUrl) {
-            sound.imageUrl = imageUrl
+    async _getImageUrls(key: ArraySoundSettings, sounds: Sound[]) {
+      if (key === 'sounds') {
+        for (const sound of sounds) {
+          if (sound.imageUrl === undefined && sound.imagePath !== undefined) {
+            const imageUrl = await this.getFile(sound.imagePath)
+            if (imageUrl) {
+              sound.imageUrl = imageUrl
+            }
           }
         }
       }
-      this[key] = sounds
       return sounds
     },
     /**
