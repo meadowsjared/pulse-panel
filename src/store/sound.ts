a851e835289488ceac6d25c153459e5da7a41a86
Jared Meadows <meadowsjared@gmail.com>
2024-05-01 15:59:00 -0700

    refactor: sound store to support multiple outputs, add volume property to Sound interface and components simplify OutputDeviceProperties definition
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/store/sound.ts
+++ src/store/sound.ts
@@ -1,24 +1,40 @@
 import { defineStore } from 'pinia'
 import chordAlert from '../assets/wav/457518__graham_makes__chord-alert-notification.wav'
-import { useSettingsStore } from './settings'
+import { SettingsStore, useSettingsStore } from './settings'
 import { Sound } from '@/@types/sound'
+
+interface OutputDeviceProperties {
+  currentAudio: HTMLAudioElement | null
+  /** true if this device is playing audio */
+  playingAudio: boolean
+  /** number of sounds that are currently playing to this device */
+  numSoundsPlaying: number
+  currentSoundResolve: (() => void) | null
+}
 
 interface State {
   volume: number
-  currentAudio: HTMLAudioElement | null
-  playingAudio: boolean
-  numSoundsPlaying: number
-  currentSoundResolve: (() => void) | null
+  outputDeviceData: OutputDeviceProperties[]
 }
 
 export const useSoundStore = defineStore('sound', {
   state: (): State => ({
     volume: 1,
-    currentAudio: null,
-    playingAudio: false,
-    numSoundsPlaying: 0,
-    currentSoundResolve: null,
+    outputDeviceData: [
+      {
+        currentAudio: null,
+        playingAudio: false,
+        numSoundsPlaying: 0,
+        currentSoundResolve: null,
+      },
+    ],
   }),
+  getters: {
+    // if any sound is playing, this will be true
+    isPlaying(): boolean {
+      return this.outputDeviceData.some((data: OutputDeviceProperties) => data.playingAudio)
+    },
+  },
   actions: {
     /**
      * Set the volume for the soundboard
@@ -27,71 +43,121 @@ export const useSoundStore = defineStore('sound', {
     async setVolume(volume: number): Promise<void> {
       this.volume = volume
     },
+    /** Populate the playing audio array
+     * @param length the length to populate it to
+     **/
+    populatePlayingAudio(length: number): void {
+      while (this.outputDeviceData.length < length) {
+        this.outputDeviceData.push({
+          currentAudio: null,
+          playingAudio: false,
+          numSoundsPlaying: 0,
+          currentSoundResolve: null,
+        })
+      }
+    },
     /**
      * Play a sound, if another sound is playing, it can stop it
-     * @param outputDeviceId device to play the sound on
+     * @param activeOutputDevices device array to play the sound on
      * @param volume volume to play the sound at, defaults to `this.volume`
      * @param audioFile the file to play, defaults to `chordAlert`
      */
     async playSound(
       audioFile: Sound | null = null,
-      outputDeviceId: string | null = null,
-      volume: number | null = null
+      activeOutputDevices: string[] | null = null,
+      selectedOutputDevices: (string | null)[] | null = null
     ): Promise<void> {
       // return a promise
       return new Promise(resolve => {
         ;(async () => {
           const settingsStore = useSettingsStore()
-          if (settingsStore.allowOverlappingSound === false && this.currentAudio && this.playingAudio) {
-            this.currentAudio.pause()
-            this.currentAudio.currentTime = 0
-            this.numSoundsPlaying--
-            if (this.currentSoundResolve) {
-              this.currentSoundResolve()
-              this.currentSoundResolve = null
-            }
+          if (!activeOutputDevices) {
+            activeOutputDevices = settingsStore.outputDevices
           }
-
-          if (audioFile?.audioUrl === undefined && audioFile?.audioPath !== undefined) {
-            const audioUrl = await settingsStore.getFile(audioFile.audioPath)
-            if (audioUrl) {
-              audioFile.audioUrl = audioUrl
-            }
+          // filter out null values
+          activeOutputDevices = activeOutputDevices.filter((deviceId): deviceId is string => deviceId !== null)
+          if (!activeOutputDevices) return
+          if (!selectedOutputDevices) {
+            selectedOutputDevices = activeOutputDevices
           }
-          this.currentAudio = new Audio(audioFile?.audioUrl ?? chordAlert)
-
-          if (!outputDeviceId) {
-            outputDeviceId = settingsStore.outputDeviceId
-            if (!outputDeviceId) {
-              return
-            }
-          }
-
-          await this.currentAudio.setSinkId(outputDeviceId).catch((error: any) => {
-            let errorMessage = error
-            if (error.name === 'SecurityError') {
-              errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`
-            }
-            console.error(errorMessage)
+          // filter out null values
+          const filteredSelectedOutputDevices = selectedOutputDevices.filter(
+            (deviceId): deviceId is string => deviceId !== null
+          )
+          activeOutputDevices.forEach(async (outputDeviceId: string) => {
+            await this._playSoundToDevice(
+              outputDeviceId,
+              audioFile?.volume ?? null,
+              resolve,
+              filteredSelectedOutputDevices,
+              settingsStore,
+              audioFile
+            )
           })
-          this.currentAudio.volume = volume ?? this.volume // set the volume to max
-          this.currentAudio.onplaying = () => {
-            this.playingAudio = true
-          }
-          this.numSoundsPlaying++
-          this.currentAudio.play()
-          this.currentSoundResolve = resolve // store this so we can end it early if we want to
-          this.currentAudio.onended = () => {
-            this.currentAudio?.remove()
-            this.currentAudio = null
-            this.numSoundsPlaying--
-            if (this.numSoundsPlaying < 1) {
-              this.playingAudio = false
-            }
-            resolve()
-          }
         })()
       })
+    },
+    /**
+     * Play a sound on a specific device
+     * @param outputDeviceId the device to play the sound on
+     * @param volume volume to play the sound at, defaults to `this.volume`
+     * @param resolve the resolve function to call when the sound is done playing
+     */
+    async _playSoundToDevice(
+      outputDeviceId: string,
+      volume: number | null,
+      resolve: () => void,
+      selectedOutputDevices: string[],
+      settingsStore: SettingsStore,
+      audioFile: Sound | null
+    ): Promise<void> {
+      const index = selectedOutputDevices.findIndex((deviceId: string | null) => deviceId === outputDeviceId)
+      if (
+        settingsStore.allowOverlappingSound === false &&
+        this.outputDeviceData[index].currentAudio &&
+        this.outputDeviceData[index].playingAudio
+      ) {
+        this.outputDeviceData[index].currentAudio.pause()
+        this.outputDeviceData[index].currentAudio.currentTime = 0
+        this.outputDeviceData[index].numSoundsPlaying--
+        if (this.outputDeviceData[index].currentSoundResolve) {
+          this.outputDeviceData[index].currentSoundResolve()
+          this.outputDeviceData[index].currentSoundResolve = null
+        }
+      }
+
+      if (audioFile?.audioUrl === undefined && audioFile?.audioPath !== undefined) {
+        const audioUrl = await settingsStore.getFile(audioFile.audioPath)
+        if (audioUrl) {
+          audioFile.audioUrl = audioUrl
+        }
+      }
+      this.outputDeviceData[index].currentAudio = new Audio(audioFile?.audioUrl ?? chordAlert)
+
+      if (!this.outputDeviceData[index].currentAudio) return
+      await this.outputDeviceData[index].currentAudio.setSinkId(outputDeviceId).catch((error: any) => {
+        let errorMessage = error
+        if (error.name === 'SecurityError') {
+          errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`
+        }
+        console.error(errorMessage)
+      })
+      this.outputDeviceData[index].currentAudio.volume = volume ?? this.volume // set the volume to max
+      this.outputDeviceData[index].currentAudio.onplaying = () => {
+        this.outputDeviceData[index].playingAudio = true
+      }
+      this.outputDeviceData[index].numSoundsPlaying++
+      this.outputDeviceData[index].currentAudio.play()
+      this.outputDeviceData[index].currentSoundResolve = resolve // store this so we can end it early if we want to
+      this.outputDeviceData[index].currentAudio.onended = () => {
+        this.outputDeviceData[index].currentAudio?.remove()
+        this.outputDeviceData[index].currentAudio = null
+        this.outputDeviceData[index].numSoundsPlaying--
+        if (this.outputDeviceData[index].numSoundsPlaying < 1) {
+          this.outputDeviceData[index].playingAudio = false
+        }
+        this.outputDeviceData[index].currentSoundResolve?.()
+      }
     },
   },
 })
