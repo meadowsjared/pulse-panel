24ed67bc645277e596c8f3ad2147690efa008018
Jared Meadows <meadowsjared@gmail.com>
2024-05-21 07:22:58 -0700

    style: Adjust height of Soundboard component to fit viewport
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/components/Soundboard.vue
+++ src/components/Soundboard.vue
@@ -122,6 +122,7 @@ function stripAudioUrls(pSounds: Sound[]) {
   grid-template-columns: repeat(auto-fill, minmax(80px, 1fr));
   grid-auto-rows: 80px;
   overflow: auto;
+  height: calc(100vh - 38px);
 }
 
 .rightSideBar {
