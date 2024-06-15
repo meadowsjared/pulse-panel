63611f90c942d44c6340762a354250d489db104d
Jared Meadows <meadowsjared@gmail.com>
2024-06-15 12:34:53 -0700

    refactor: copySourceSoundsToDestination logic to be more robust
    The commit updates the logic in the `copySourceSoundsToDestination` function in the `copySoundsToDest.js` file. It improves the matching and updating of sound objects between the source and destination arrays, so even if they're in a different order, as long as there is only one with that title, it will match it. Additionally, it adds better handling for missing sounds and ensures that all sounds are in the correct order. The changes enhance the functionality and accuracy of the sound copying process.
    
    Note: The commit message has been generated based on the provided code changes and recent commits.
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- config-utils/copySoundsToDest.js
+++ config-utils/copySoundsToDest.js
@@ -44,38 +44,70 @@ function copySourceSoundsToDestination(sourcePath, destinationPath) {
 
   const fieldsToCopy = ['id', 'hideTitle', 'hotkey', 'isPreview', 'tags', 'volume']
 
-  let changedSounds = 0
-  destinationSounds.forEach((sound, index) => {
+  let count = {
+    found: 0,
+    notFound: 0,
+    changed: 0,
+  }
+  /** keeps track of whether all the sounds match, and are in the same order */
+  let allMatch = true
+  destinationSounds.forEach((destinationSound, index) => {
     // find the sound in the source object where the name matches
+    /**
+     * @type {Sound}
+     */
+    const sourceSound = sourceSounds.find(sound => sound.title === destinationSound.title)
     if (
-      sourceSounds[index] &&
       destinationSounds[index] &&
-      destinationSounds[index].title === sourceSounds[index].title
+      destinationSounds.filter(sound => sound.title === sourceSounds[index].title).length === 1
     ) {
-      console.log('checked', sourceSounds[index].id)
+      count.found++
       // if the sound exists in the source object, update it
       let changed = false
       fieldsToCopy.forEach(key => {
         if (
-          sourceSounds[index][key] !== undefined &&
-          JSON.stringify(sourceSounds[index][key]) !== JSON.stringify(destinationSounds[index][key])
+          sourceSound[key] !== undefined &&
+          JSON.stringify(sourceSound[key]) !== JSON.stringify(destinationSounds[index][key])
         ) {
           changed = true
-          console.log('copying', key, sourceSounds[index][key])
-          destinationSounds[index][key] = sourceSounds[index][key]
+          console.log('copying', `'${sourceSound.title}'`, key, sourceSound[key])
+          destinationSounds[index][key] = sourceSound[key]
+        } else if (sourceSound[key] === undefined && destinationSounds[index][key] !== undefined) {
+          console.log('deleting', `'${sourceSound.title}'`, key, destinationSounds[index][key])
+          changed = true
+          delete destinationSounds[index][key]
         }
       })
       if (changed) {
-        changedSounds++
+        count.changed++
       }
     } else {
-      console.log('sound not found', sound.title)
+      allMatch = false
+      count.notFound++
+      console.log('sound not found', destinationSound.title, destinationSound.id)
     }
   })
-  console.log('changedSounds:', changedSounds)
+  sourceSounds.forEach((_, index) => {
+    if (destinationSounds[index].title !== sourceSounds[index].title) {
+      console.log(`${sourceSounds[index].title} not in correct location`)
+      allMatch = false
+    }
+  })
 
-  destinationObject.sounds = JSON.stringify(destinationSounds)
-  fs.writeFileSync(`${destinationPath}`, JSON.stringify(destinationObject, null, 2))
+  console.log('results:', count)
+  if (count.notFound > 0) {
+    console.log(`WARNING! ${count.notFound} sounds not found`)
+  }
+  // only write the file if there were changes
+  if (count.changed > 0) {
+    destinationObject.sounds = JSON.stringify(destinationSounds)
+    fs.writeFileSync(`${destinationPath}`, JSON.stringify(destinationObject, null, 2))
+  }
+  if (allMatch) {
+    console.log('All sounds match!')
+  } else {
+    console.log('WARNING! Sounds do not match')
+  }
 }
 
 module.exports = { copySourceSoundsToDestination }
