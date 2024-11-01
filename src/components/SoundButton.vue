df6f2927ccf5dae1fea572e9415207fb31abe8db
Jared Meadows <meadowsjared@gmail.com>
2024-10-31 18:04:23 -0700

    chore: improve color handling in SoundButton component now it will only add the color if it is set
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

--- src/components/SoundButton.vue
+++ src/components/SoundButton.vue
@@ -24,7 +24,7 @@
       <span
         v-if="!props.modelValue.hideTitle"
         class="button-title"
-        :style="`color: ${props.modelValue.color ?? '#ffffff'}`"
+        :style="props.modelValue.color && `color: ${props.modelValue.color}`"
         >{{ modelValue.title || 'New Sound' }}</span
       >
     </button>
